c83be63c38f693e9091bec9615f7f597ead25312
map <port02.server@gmail.com>
2019-05-12 21:56:20 +0100

    update version

--- party.py
+++ party.py
@@ -1,6 +1,6 @@
-#This file is part party_vat_unique module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part party_vat_unique module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 
 from trytond.pool import Pool, PoolMeta
@@ -31,15 +31,15 @@ class PartyIdentifier(metaclass=PoolMeta):
             identifier.unique_code()
 
     def unique_code(self):
-        #Warn on existing code and type
+        # Warn on existing code and type
         user = Transaction().user
 
-        if (self.id > 0 and user > 1):
+        if self.id > 0 and user > 1:
             identifiers = Pool().get('party.identifier')
-            identifiers_count = identifiers.search_count(
-                [('code', '=', self.code),('type', '=', 'eu_vat'),])
-            if (identifiers_count > 1):
-                self.raise_user_warning('warn_identifier_with_same_code.%d' % self.id,
-                    'There is another code with the same number: %s', self.rec_name)
-
-
+            identifiers_count = identifiers.search_count([('code', '=', self.code), ('type', '=', 'eu_vat')])
+            if identifiers_count > 1:
+                self.raise_user_warning(
+                    'warn_identifier_with_same_code.%d' % self.id,
+                    'There is another code with the same number: %s',
+                    self.rec_name,
+                )
